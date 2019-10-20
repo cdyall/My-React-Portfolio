35e25393d24e8e0fa38b9a9b7b03b71fc391e595
cdyall <cdyall1@gmail.com>
2019-10-19 20:52:10 -0400

    first commit

---
+++ client/src/components/homepage.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div style={{ width: "100%", margin: "auto" }}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png" alt="Avatar" className="avatar-img" />
+
+                        <div className="banner-text">
+
+                            <h1>Full Stack Web Developer</h1>
+
+                            <hr />
+
+                            <p>HTML/CSS | Bootstrap | Javascript | React | NodeJS | MYSQL | MongoDB</p>
+
+                            <div className="social-links">
+                                
+                                {/* Github */}
+                                <a href="https://github.com/cdyall/" rel="noopener noreferrer" target="_blank" > <i className="fa fa-github-square" aria-hidden="true" /> </a>
+
+                                {/* Stack Overflow*/}
+                                <a href="https://stackoverflow.com/users/12244146/dyall" rel="noopener noreferrer" target="_blank" > <i className="fa fa-stack-overflow" aria-hidden="true" /> </a>
+
+                                {/* Reddit*/}
+                                <a href="https://www.reddit.com/user/CDyall" rel="noopener noreferrer" target="_blank" > <i className="fa fa-reddit-square" aria-hidden="true" /> </a>
+
+                                {/* Linkedin */}
+                                <a href="https://www.linkedin.com/in/chris-dyall-aa0bba78/" rel="noopener noreferrer" target="_blank" > <i className="fa fa-linkedin-square" aria-hidden="true" /> </a>
+
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
+
+        )
+    }
+}
+
+
+export default Home;
---
+++ client/src/components/main.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom"
+
+import Home from "./homepage";
+import Resume from "./resume";
+import About from "./aboutme";
+import Project from "./projects";
+import Contact from "./contact";
+
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/resume" component={Resume}/>
+        <Route path="/aboutme" component={About}/>
+        <Route path="/projects" component={Project}/>
+        <Route path="/contact" component={Contact}/>
+    </Switch>
+
+)
+
+export default Main;
